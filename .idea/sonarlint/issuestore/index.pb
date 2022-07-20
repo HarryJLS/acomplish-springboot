e6aea7fb3a3b5da5d3c74b9df38031209b7d7027
JLS <758065565@qq.com>
2022-07-20 22:00:46 +0800

    jdk类加载器测试

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+l
+<user/src/main/java/com/example/user/service/UserService.java,b\d\bd981488f3800f8d3426dde76db25456b1317c76
---
+++ jdk_pratice/src/main/java/myclassloader/file/User.java
@@ -0,0 +1,16 @@
+package myclassloader.file;
+
+/**
+ * @author JLS
+ * @description: 测试类
+ * @since 2022-07-20 20:45
+ */
+public class User {
+
+    static {
+        System.out.println("调用类成功");
+    }
+    public static void sout() {
+        System.out.println("shishikakn");
+    }
+}
